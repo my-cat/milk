42b47132e6856ebe8e9449aeb74e7a83c951fa3c
my_cat <huangjinchao2011@gmail.com>
2013-10-18 08:15:00 +0800

    heroku

--- src/milk/core.clj
+++ src/milk/core.clj
@@ -9,10 +9,4 @@
   (println "aaaaaaaaaaaaa")
     (println "aaaaaaaaaaaaa")
       (println "aaaaaaaaaaaaa")
-        (println "aaaaaaaaaaaaa")
-
-(mc/ensure-index "pastes" {:user 1 :date 1})
-(mc/ensure-index "pastes" {:private 1})
-(mc/ensure-index "pastes" {:id 1})
-(mc/ensure-index "pastes" {:paste-id 1})
-(mc/ensure-index "pastes" {:fork 1}))
+        (println "aaaaaaaaaaaaa"))
